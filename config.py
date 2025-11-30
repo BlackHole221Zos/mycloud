654e7a3906a5b77eac510379afa8ae9f31e6d6df
BlackHole221 <deniszosimov4@gmail.com>
2025-11-30 14:47:25 +0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-    MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 500 MB
-    DATABASE = os.path.join(BASE_DIR, 'cloud.db')
